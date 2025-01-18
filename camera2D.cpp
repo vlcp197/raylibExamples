e0f2e3ada044875945b1358e72277ce19bb424a0
vlcp197 <vlcp197@gmail.com>
2025-01-18 14:58:19 -0300

    Write 2d camera code

--- camera2D.cpp
+++ camera2D.cpp
@@ -1,7 +1,7 @@
 #include "camera2D.h"
 
-int camera2D(void)
-{
+int camera2D(void){
+
     const int screenWidth = 800;
     const int screenHeight = 450;
 
@@ -13,8 +13,7 @@ int camera2D(void)
 
     int spacing = 0;
 
-    for (int i = 0; i < MAX_BUILDINGS; i++)
-    {
+    for (int i = 0; i < MAX_BUILDINGS; i++){
         buildings[i].width = (float)GetRandomValue(50, 200);
         buildings[i].height = (float)GetRandomValue(100, 800);
         buildings[i].y = screenHeight - 130.0f - buildings[i].height;
@@ -25,11 +24,71 @@ int camera2D(void)
         buildColors[i] = (Color){GetRandomValue(200, 240), GetRandomValue(200, 240), GetRandomValue(200, 250), 255};
     }
 
-    
+    Camera2D camera = {0};
+    camera.target = (Vector2){ player.x + 20.0f, player.y + 20.0f };
+    camera.offset = (Vector2){ screenWidth/2.0f, screenHeight/2.0f };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
 
-    while (!WindowShouldClose())
-    {
+    SetTargetFPS(60);
 
+    while (!WindowShouldClose()){
+
+        if (IsKeyDown(KEY_RIGHT)) player.x += 2;
+        else if (IsKeyDown(KEY_LEFT)) player.x -= 2;
+
+        camera.target = (Vector2){ player.x + 20, player.y + 20 };
+
+        if (IsKeyDown(KEY_A)) camera.rotation--;
+        else if (IsKeyDown(KEY_S)) camera.rotation++;
+
+        if (camera.rotation > 40) camera.rotation = 40;
+        else if (camera.rotation < -40) camera.rotation = -40;
+
+        camera.zoom += ((float) GetMouseWheelMove()*0.05f);
+
+        if (camera.zoom > 3.0f) camera.zoom = 3.0f;
+        else if (camera.zoom < 0.1f) camera.zoom = 0.1f;
+
+        if (IsKeyPressed(KEY_R)){
+            camera.zoom = 1.0f;
+            camera.rotation = 0.0f;
+        }
+
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            BeginMode2D(camera);
+                DrawRectangle(-6000, 320, 13000, 8000, DARKGRAY);
+
+                for (int i = 0; i < MAX_BUILDINGS; i++){
+                    DrawRectangleRec(buildings[i], buildColors[i]);
+                }
+
+                DrawRectangleRec(player, RED);
+
+                DrawLine((int)camera.target.x, -screenHeight*10, (int)camera.target.x, screenHeight*10, GREEN);
+
+                DrawLine(-screenWidth*10, (int)camera.target.y, screenWidth*10, (int)camera.target.y, GREEN);
+        
+            EndMode2D();
+
+            DrawText("SCREEN AREA", 640, 10, 20, RED);
+
+            DrawRectangle(0, 0, screenWidth, 5, RED);
+            DrawRectangle(0,5, 5, screenHeight - 10, RED);
+            DrawRectangle(screenWidth - 5, 5, 5, screenHeight - 10, RED);
+            DrawRectangle(0,screenHeight -5, screenWidth , 5, RED );
+
+            DrawRectangle(10, 10, 250, 113, Fade(SKYBLUE, 0.5f));
+            DrawRectangleLines(10, 10, 250, 113, BLUE);
+
+            DrawText("Free 2d camera controls:", 20, 20, 10, BLACK);
+            DrawText("- Right/Left to move Offset", 40, 40, 10, DARKGRAY);
+            DrawText("- Mouse Wheel to Zoom in-out", 40, 60, 10, DARKGRAY);
+            DrawText("- A / S to Rotate", 40, 80, 10, DARKGRAY);
+            DrawText("- R to reset Zoom and Rotation", 40, 100, 10, DARKGRAY);
+
+        EndDrawing();
     }
 
     return 0;    
